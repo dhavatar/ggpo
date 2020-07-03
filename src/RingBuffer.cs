93e41b2ed86cf1fdb776d11c3ec3b4fe83988d6f
David Hsu <dahsboot@gmail.com>
2020-07-02 18:38:22 -0700

    Converted RingBuffer and StaticBuffer to C# - Removed converted files and other files not needed for the C# version

---
+++ src/RingBuffer.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics;
+
+namespace GGPOSharp
+{
+    /// <summary>
+    /// Generic ring buffer.
+    /// </summary>
+    /// <typeparam name="T">Type of item to store in the ring buffer.</typeparam>
+    public class RingBuffer<T>
+    {
+        /// <summary>
+        /// Current number of items in the ring buffer.
+        /// </summary>
+        public int Size
+        {
+            get
+            {
+                if (head >= tail)
+                {
+                    return head - tail;
+                }
+                return head + (Capacity - tail);
+            }
+        }
+
+        /// <summary>
+        /// Total capacity of the ring buffer.
+        /// </summary>
+        public int Capacity => data.Length;
+
+        /// <summary>
+        /// Returns true if there's no item in the buffer.
+        /// </summary>
+        public bool IsEmpty => Size == 0;
+
+        /// <summary>
+        /// Returns true if the amount of items fills the buffer.
+        /// </summary>
+        public bool IsFull => Size == Capacity;
+
+        protected readonly T[] data;
+        protected int head;
+        protected int tail;
+
+        /// <summary>
+        /// Constructor for the ring buffer with the specified size.
+        /// </summary>
+        /// <param name="size">Size of the ring buffer.</param>
+        public RingBuffer(int size)
+        {
+            data = new T[size];
+            head = tail = 0;
+        }
+
+        /// <summary>
+        /// Returns the front item in the ring buffer.
+        /// </summary>
+        /// <returns>Front item in the buffer.</returns>
+        public ref T Front()
+        {
+            Debug.Assert(Size != Capacity);
+            return ref data[tail];
+        }
+
+        /// <summary>
+        /// Retrieves the i-th item in the ring buffer.
+        /// </summary>
+        /// <param name="i">Index in the ring buffer.</param>
+        /// <returns>Item located at the specified index.</returns>
+        public ref T this[int i]
+        {
+            get
+            {
+                Debug.Assert(i < Size);
+                return ref data[(tail + i) % Capacity];
+            }
+        }
+
+        /// <summary>
+        /// Removes the front item in the ring buffer.
+        /// </summary>
+        public void Pop()
+        {
+            Debug.Assert(Size != Capacity);
+            tail = (tail + 1) % Capacity;
+        }
+
+        /// <summary>
+        /// Adds an item to the back of the ring buffer.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Push(in T item)
+        {
+            Debug.Assert(Size != Capacity - 1);
+            data[head] = item;
+            head = (head + 1) % Capacity;
+        }
+    }
+}
---
+++ src/StaticBuffer.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace GGPOSharp
+{
+    /// <summary>
+    /// Generic static buffer array.
+    /// </summary>
+    /// <typeparam name="T">Type of item for the buffer to use.</typeparam>
+    public class StaticBuffer<T>
+    {
+        protected T[] data;
+
+        /// <summary>
+        /// The amount of items currently in the buffer.
+        /// </summary>
+        public int Size { get; protected set; }
+
+        /// <summary>
+        /// Constructor to create the buffer to the specified size.
+        /// </summary>
+        /// <param name="size">Size of the buffer.</param>
+        public StaticBuffer(int size)
+        {
+            data = new T[size];
+            Size = 0;
+        }
+
+        /// <summary>
+        /// Access the buffer at the specified index.
+        /// </summary>
+        /// <param name="i">Index in the buffer.</param>
+        /// <returns>Item from the specified index.</returns>
+        public ref T this[int i]
+        {
+            get
+            {
+                Debug.Assert(i >= 0 && i < Size);
+                return ref data[i];
+            }
+        }
+
+        /// <summary>
+        /// Adds an item to the buffer.
+        /// </summary>
+        /// <param name="item">Item to add to the buffer.</param>
+        public void Push(in T item)
+        {
+            Debug.Assert(Size != data.Length - 1);
+            data[Size++] = item;
+        }
+    }
+}
